6182ee5b85cc4ac290a84aeb20c5f80698a9798e
JEnriqueKu <104782947+JEnriqueKu@users.noreply.github.com>
2023-05-09 10:49:44 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,15 @@ public class Main {
         //Exercise3 exercise3 = new Exercise3();
         //Exercise4 exercise4 = new Exercise4();
         //Exercise5 exercise5 = new Exercise5();
-        Exercise6 exercise6 = new Exercise6();
+        //Exercise6 exercise6 = new Exercise6();
         //Exercise7 exercise7 = new Exercise7();
+        
+        //PTS_Exercise1 Ex1 = new PTS_Exercise1();
+        //PTS_Exercise2 Ex2 = new PTS_Exercise2();
+        //PTS_Exercise3 Ex3 = new PTS_Exercise3();
+        //PTS_Exercise4 Exercise4 = new PTS_Exercise4();
+        //PTS_Exercise5 Exercise5  = new PTS_Exercise5();
+        //PTS_Exercise6 Exercise6 = new PTS_Exercise6();
 
     }
 }
